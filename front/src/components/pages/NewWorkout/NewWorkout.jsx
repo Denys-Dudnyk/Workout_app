25745c6a2c2b1670129a305565815a7fe9f42ba6
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2022-10-05 16:48:38 +0200

    Front-End || Data output from the database in React Select

--- front/src/components/pages/NewWorkout/NewWorkout.jsx
+++ front/src/components/pages/NewWorkout/NewWorkout.jsx
@@ -7,28 +7,67 @@ import { Link } from 'react-router-dom'
 import bgImage from '../../../images/new-workout-bg.jpg'
 import Field from '../../ui/Field/Field'
 import Button from '../../ui/Button/Button'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { $api } from '../../../api/api'
+import Alert from '../../ui/Alert/Alert'
+import Loader from '../../ui/Loader'
 
 //const animatedComponents = makeAnimated()
 
 const NewWorkout = () => {
 	//const navigate = useNavigate()
 	const [name, setName] = React.useState('')
-	const [exercises, setExercises] = React.useState([])
+	const [exercisesCurrent, setExercisesCurrent] = React.useState([])
 
-	const handleSubmit = () => {
-		console.log('submit')
+	const { data, isSuccess } = useQuery(
+		['List exercises'],
+		() =>
+			$api({
+				url: '/exercises',
+			}),
+		{
+			refetchOnWindowFocus: false,
+		}
+	)
+
+	const {
+		mutate,
+		isSuccess: isSuccesMutate,
+		isLoading,
+		error,
+	} = useMutation(
+		['Create new Workout'],
+		({ exIds }) =>
+			$api({
+				url: '/workouts',
+				type: 'POST',
+				body: { name, exerciseIds: exIds },
+			}),
+		{
+			onSuccess() {
+				setName('')
+				setExercisesCurrent('')
+			},
+		}
+	)
+
+	const handleSubmit = e => {
+		e.preventDefault()
+
+		const exIds = exercisesCurrent.map(ex => ex.value)
+
+		mutate({
+			exIds,
+		})
 	}
-
-	const options = [
-		{ value: 0, label: 'Push-ups' },
-		{ value: 1, label: 'Pull-ups' },
-		{ value: 2, label: 'Puls-ups' },
-	]
 
 	return (
 		<>
 			<Layout bgImage={bgImage} heading='Create new workout' />
 			<div className='wrapper-inner-page'>
+				{error && <Alert type='error' text={error} />}
+				{isSuccesMutate && <Alert text='Workout created' />}
+				{isLoading && <Loader />}
 				<form onSubmit={handleSubmit}>
 					<Field
 						placeholder='Enter name'
@@ -39,18 +78,22 @@ const NewWorkout = () => {
 					<Link to='/new-exercise' className='dark-link'>
 						Add new exercise
 					</Link>
-					<Select
-						//components={animatedComponents}
-						classNamePrefix='select2-selection'
-						closeMenuOnSelect={false}
-						placeholder='Exercises...'
-						title='Exercises'
-						isMulti
-						options={options}
-						value={exercises}
-						onChange={setExercises}
-					/>
-
+					{isSuccess && data && (
+						<Select
+							//components={animatedComponents}
+							classNamePrefix='select2-selection'
+							closeMenuOnSelect={false}
+							placeholder='Exercises...'
+							title='Exercises'
+							isMulti
+							options={data.map(ex => ({
+								value: ex._id,
+								label: ex.name,
+							}))}
+							value={exercisesCurrent}
+							onChange={setExercisesCurrent}
+						/>
+					)}
 					<Button text='Create' callback={() => {}} />
 				</form>
 			</div>
