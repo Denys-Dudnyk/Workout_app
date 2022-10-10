42997e93b9e24ced0e6b15f4d54f935347a13c74
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2022-10-11 00:31:46 +0200

    Front-End || Delete Workout

--- front/src/components/pages/Workouts/ListWorkouts.jsx
+++ front/src/components/pages/Workouts/ListWorkouts.jsx
@@ -1,14 +1,40 @@
 import bgImage from '../../../images/workout_bg.jpg'
 import styles from './SingleWorkout.module.scss'
 
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { $api } from '../../../api/api'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Alert from '../../ui/Alert/Alert'
 import Layout from '../../common/Layout'
+import { AiFillDelete } from 'react-icons/ai'
+import Loader from '../../ui/Loader'
 
 const ListWorkouts = () => {
-	const { data, isSuccess } = useQuery(
+	const navigate = useNavigate()
+	const {
+		mutate,
+		isSuccess: isSuccesMutate,
+		isLoading,
+	} = useMutation(
+		({ workId }) =>
+			$api({
+				url: `/workouts/${workId}`,
+				type: 'DELETE',
+			}),
+		{
+			onSuccess() {
+				refetch()
+			},
+		}
+	)
+
+	const handleDelete = id => {
+		mutate({
+			workId: id,
+		})
+	}
+
+	const { data, isSuccess, refetch } = useQuery(
 		['Get Workouts'],
 		() =>
 			$api({
@@ -25,13 +51,23 @@ const ListWorkouts = () => {
 				className='wrapper-inner-page'
 				style={{ paddingLeft: 0, paddingRight: 0 }}
 			>
+				{isLoading && <Loader />}
+				{isSuccesMutate && <Alert text='Workout removed' />}
 				{isSuccess ? (
 					<div className={styles.wrapper}>
 						{data.map((workout, idx) => (
 							<div className={styles.item} key={`workout ${idx}`}>
-								<Link to={`/workouts/${workout._id}`}>
-									<span>{workout.name}</span>
-								</Link>
+								{/* <Link to={`/workouts/${workout._id}`}> */}
+								<span onClick={() => navigate(`/workouts/${workout._id}`)}>
+									{workout.name}
+								</span>
+								<AiFillDelete
+									className={styles.delete}
+									size='1.5em'
+									title='Delete'
+									onClick={() => handleDelete(workout._id)}
+								/>
+								{/* </Link> */}
 							</div>
 						))}
 					</div>
--- front/src/components/pages/Workouts/SingleWorkout.jsx
+++ front/src/components/pages/Workouts/SingleWorkout.jsx
@@ -7,11 +7,12 @@ import stylesLayout from '../../common/Layout.module.scss'
 
 import { useQuery } from '@tanstack/react-query'
 import { $api } from '../../../api/api'
-import { Link, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import Alert from '../../ui/Alert/Alert'
 
 const SingleWorkout = () => {
 	const { id } = useParams()
+	const navigate = useNavigate()
 
 	const { data, isSuccess } = useQuery(
 		['Get Workout'],
@@ -48,15 +49,17 @@ const SingleWorkout = () => {
 							return (
 								<Fragment key={`ex ${idx}`}>
 									<div className={styles.item}>
-										<Link to={`exercises/${ex._id}`}>
-											<span>{ex.name}</span>
-											<img
-												src={`/uploads/exercises/${ex.imageName}.svg`}
-												height='34'
-												alt=''
-												draggable={false}
-											/>
-										</Link>
+										{/* <Link to={`exercises/${ex._id}`}> */}
+										<span onClick={() => navigate(`exercises/${ex._id}`)}>
+											{ex.name}
+										</span>
+										<img
+											src={`/uploads/exercises/${ex.imageName}.svg`}
+											height='34'
+											alt=''
+											draggable={false}
+										/>
+										{/* </Link> */}
 									</div>
 									{idx % 2 !== 0 && <div className={styles.line}></div>}
 								</Fragment>
