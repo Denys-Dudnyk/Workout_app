4db89f9149ef7c3af2c793c520043e86f9d5a34e
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2022-10-19 21:47:15 +0200

    Back-End || Fix Back-End

--- back/routes/workoutRoutes.js
+++ back/routes/workoutRoutes.js
@@ -1,5 +1,9 @@
 import express from 'express'
-import { createNewWorkoutLog } from '../controllers/workout/logController.js'
+import {
+	createNewWorkoutLog,
+	getWorkoutLog,
+	updateCompleteWorkoutLog,
+} from '../controllers/workout/logController.js'
 import {
 	createNewWorkout,
 	deleteWorkout,
@@ -17,7 +21,11 @@ router
 	.get(protect, getWorkouts)
 	.post(protect, createNewWorkout)
 	.put(protect, updateWorkout)
+	.delete(protect, deleteWorkout)
+
 router.route('/log').post(protect, createNewWorkoutLog)
+router.route('/log/completed').put(protect, updateCompleteWorkoutLog)
 router.route('/:id').get(protect, getWorkout).delete(protect, deleteWorkout)
+router.route('/log/:id').get(protect, getWorkoutLog)
 
 export default router
