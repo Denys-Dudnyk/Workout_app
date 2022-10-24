bf8f728da67ab77d40a023f3c892f3de3492a50b
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2022-10-24 10:17:40 +0200

    Update server.js

--- back/server.js
+++ back/server.js
@@ -40,10 +40,10 @@ app.use(errorHandler)
 
 if (process.env.NODE_ENV === 'production') {
 	// Step 1:
-	app.use(express.static(path.resolve(__dirname, './front/build')))
+	app.use(express.static(path.resolve(__dirname, '../front/build')))
 	// Step 2:
 	app.get('*', function (request, response) {
-		response.sendFile(path.resolve(__dirname, './front/build', 'index.html'))
+		response.sendFile(path.resolve(__dirname, '../front/build', 'index.html'))
 	})
 }
 
